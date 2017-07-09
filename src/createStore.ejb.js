aed315493a9d876905a7b3106012a9fc9017835d
EJ Brennan <ejbrennan@gmail.com>
2017-07-09 17:55:09 -0400

    Created a sample implementation of createStore and reference it in index.js

---
+++ src/createStore.ejb.js
@@ -0,0 +1,25 @@
+
+// This is a simplified implementation of the redux createStore function
+
+export const createStore = (reducer) => {
+    let state;
+    let listeners = [];
+
+    const getState = ()=>state;
+    
+    const dispatch = (action)=>{
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    };
+
+    const subscribe = (listener)=>{
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener);
+        };
+    };
+
+    dispatch({});
+    
+    return { getState, dispatch, subscribe };
+};
